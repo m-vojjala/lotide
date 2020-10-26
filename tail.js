515bbc3d202b71e99b6294b95685322378fe1feb
MounikaVojjala <mounikavojjala@gmail.com>
2020-10-26 21:56:49 +0000

    tail of an array

---
+++ tail.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual}  ==== ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual}  !==  ${expected}`);
+  }
+
+};
+
+const tail = function(arr) {
+  return arr.slice(1);
+};
+
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
