ac57a1c65985dc0ead29afb0c03958366c2e190f
MounikaVojjala <mounikavojjala@gmail.com>
2020-10-27 22:15:07 +0000

    checked equality of arrays

--- head.js
+++ head.js
@@ -9,9 +9,9 @@ const assertEqual = function(actual, expected) {
 // assertEqual("Lighthouse Labs", "Bootcamp");
 // assertEqual(1, 1);
 
-const head = function(arr){
+const head = function(arr) {
   return arr[0];
-}
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
