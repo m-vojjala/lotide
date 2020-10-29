762a2bfdb095989efd4900664fde441996a24f73
MounikaVojjala <mounikavojjala@gmail.com>
2020-10-29 21:09:45 +0000

    added findKey

---
+++ findkey.js
@@ -0,0 +1,43 @@
+const { Console } = require("console");
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual}  ==== ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual}  !==  ${expected}`);
+  }
+
+};
+
+
+
+const findKey = function(obj,callback){
+// let valuesArray = Object.values(obj);
+// console.log(valuesArray)
+for(let key in obj){
+  // console.log(callback(obj[key]))
+   if(callback(obj[key])){
+   return key;
+   }
+}
+return key
+
+}
+
+
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); 
+assertEqual(findKey({"Blue Hill": { stars: 1 },
+"Akaleri":   { stars: 3 },
+"noma":      { stars: 2 },
+"elBulli":   { stars: 3 },
+"Ora":       { stars: 2 },
+"Akelarre":  { stars: 3 }
+}, x => x.stars === 2),"noma")
