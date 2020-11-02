0dd6a5b63da6db68860612ac47621014d623a8ad
MounikaVojjala <mounikavojjala@gmail.com>
2020-11-02 18:23:06 +0000

    eqArrys refactored

---
+++ test/eqArraysTest.js
@@ -0,0 +1,10 @@
+const eqArrays = require('../eqArrays')
+
+const assertEqual =  require('../assertEqual')
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3])))
+eqArrays([1, 2, 3], [3, 2, 1])
+
+
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]));
